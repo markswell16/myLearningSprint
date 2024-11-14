e16ebf0f25341455bfc8dae1a9313c97470cc543
Maxwell <markswell16>
2024-11-14 20:53:58 +0000

    learningjavascript

---
+++ index.js
@@ -0,0 +1,32 @@
+let a = 1;
+let b = 2;
+let answer = a + b;
+console.log(answer);
+
+let c = 576;
+let d = 345;
+let result = c * d;
+console.log(result);
+
+let person = {
+    name: 'Maxwell',
+    age: 45
+};
+console.log(person);
+
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2]= 'green'
+console.log(selectedColors.length);
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+greet('John', 'smith');
+greet('mary');
+
+
+function square(number) {
+    return number * number;
+}
+console.log(square(2));
