449d05c18b76b950d513e0e79e5537ac0c45eb49
Maxwell <ifeanyi_markswell@yahoo.com>
2024-12-20 11:39:04 +0000

    commit message

--- index.js
+++ index.js
@@ -9,55 +9,93 @@ let result = c * d;
 console.log(result);
 
 let person = {
-    name: 'Maxwell',
-    age: 45
+  name: "Maxwell",
+  age: 45,
 };
 console.log(person);
 
-
-let selectedColors = ['red', 'blue'];
-selectedColors[2]= 'green'
+let selectedColors = ["red", "blue"];
+selectedColors[2] = "green";
 console.log(selectedColors.length);
 
 function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName);
+  console.log("Hello " + name + " " + lastName);
 }
-greet('John', 'smith');
-greet('mary');
-
+greet("John", "smith");
+greet("mary");
 
 function square(number) {
-    return number * number;
+  return number * number;
 }
 console.log(square(2));
 
-console.log(7)
+console.log(7);
 
 let h = 111;
 let g = 211;
 let object = h * g;
 console.log(object);
 
-alert('Hello World!')
-undefined
-let fx = 'awesome'
-undefined
-if (fx === 'awesome') alert('javaScript is FUN!') 
-undefined
-fx = 'boring'
-'boring'
-if (fx === 'awesome') alert('javaScript is FUN!')
-undefined
-34+34+76-56
-88
+alert("Hello World!");
+undefined;
+let fx = "awesome";
+undefined;
+if (fx === "awesome") alert("javaScript is FUN!");
+undefined;
+fx = "boring";
+("boring");
+if (fx === "awesome") alert("javaScript is FUN!");
+undefined;
+34 + 34 + 76 - 56;
+88;
 
+console.log("Jonas");
 
-console.log('Jonas');
-
-let firstName = 'messi';
+let firstName = "messi";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-console.log('hello')
-console.log('hi')
+console.log("hello");
+console.log("hi");
+console.log("");
+console.log("");
+'hello'
+'hello'
+alert('hello');
+undefined
+'some' + 'text'
+'sometext'
+'some' + 'more' + 'text'
+'somemoretext'
+typeof 2
+'number'
+typeof 'hello'
+'string'
+'hello' + 3
+'hello3'
+'$' + 20.95 + 7.99
+'$20.957.99'
+'$' + (20.95 + 7.99) 
+'$28.939999999999998'
+'$' + (2095+799)/100
+'$28.94'
+'items ( ' + (1 + 1) + '): $' + (2095+799)/100
+'items ( 2): $28.94'
+alert('items ( ' + (1 + 1) + '): $' + (2095+799)/100)
+undefined
+"i'm learning JavaScript"
+"i'm learning JavaScript"
+'i\'m leanring Java Script'
+"i'm leanring Java Script"
+alert('some\ntext');
+undefined
+`hello`
+'hello'
+`Items(${1 + 1}): $${(2095 = 799) /100}`
+VM2385:1 Uncaught SyntaxError: Invalid left-hand side in assignmentUnderstand this errorAI
+`Items (${1 + 1}): $${(2095 + 799) /100}`
+'Items (2): $28.94'
+`some
+text`
+'some\ntext'
