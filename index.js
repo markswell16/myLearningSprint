bdd3a49c36bd332480edd5beb45cd8daea015ec4
Maxwell <ifeanyi_markswell@yahoo.com>
2025-01-13 10:56:53 +0000

    commit message

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
 
@@ -9,14 +11,37 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+//Variable name convention
 let jonas_martin = "JM";
 let $function = 27;
 
 let person = "jonas";
 let PI = 3.1415;
 
-let myFiratJob = "programmer";
+let myFirstJob = "coder";
 let myCurrentJob = "teacher";
 
 job1 = "programmer";
 job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+let JavaScriptIsFun = true;
+console.log(JavaScriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof JavaScriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "jonas");
+
+JavaScriptIsFun = "YES!";
+console.log(typeof JavaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
