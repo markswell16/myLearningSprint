36031baee6af03a524fa8357153c7bdf8ef16080
Maxwell <ifeanyi_markswell@yahoo.com>
2025-01-13 10:11:10 +0000

    commit message

--- index.js
+++ index.js
@@ -1,53 +1,5 @@
-let a = 1;
-let b = 2;
-let answer = a + b;
-console.log(answer);
-
-let c = 576;
-let d = 345;
-let result = c * d;
-console.log(result);
-
-let person = {
-  name: "Maxwell",
-  age: 45,
-};
-console.log(person);
-
-let selectedColors = ["red", "blue"];
-selectedColors[2] = "green";
-console.log(selectedColors.length);
-
-function greet(name, lastName) {
-  console.log("Hello " + name + " " + lastName);
-}
-greet("John", "smith");
-greet("mary");
-
-function square(number) {
-  return number * number;
-}
-console.log(square(2));
-
-console.log(7);
-
-let h = 111;
-let g = 211;
-let object = h * g;
-console.log(object);
-
-alert("Hello World!");
-undefined;
-let fx = "awesome";
-undefined;
-if (fx === "awesome") alert("javaScript is FUN!");
-undefined;
-fx = "boring";
-("boring");
-if (fx === "awesome") alert("javaScript is FUN!");
-undefined;
-34 + 34 + 76 - 56;
-88;
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 
@@ -56,7 +8,15 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-console.log("hello");
-console.log("hi");
-console.log("");
-console.log("");
+
+let jonas_martin = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFiratJob = "programmer";
+let myCurrentJob = "teacher";
+
+job1 = "programmer";
+job2 = "teacher";
